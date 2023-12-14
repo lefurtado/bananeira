68a6e3db894073964df1daa23cd5be89ac776cee
Leandro Furtado <le_furtado@outlook.com.br>
2023-12-14 19:35:01 -0300

    tamanho do modal de add fornecedor, removido select

--- Components/bananeira/ModalFornecedores.tsx
+++ Components/bananeira/ModalFornecedores.tsx
@@ -7,7 +7,7 @@ const ModalFornecedores = ({ showModal, onHide, finalState }) => {
         show={showModal}
         onHide={onHide}
         centered
-        size='lg'
+        size='md'
       >
         <Modal.Header className="modal-title fw-bold">
           Adicionar Fornecedores
@@ -15,36 +15,23 @@ const ModalFornecedores = ({ showModal, onHide, finalState }) => {
         <Modal.Body>
           <Form action="#">
             <Row className="g-3">
-              <Col lg={6}>
+              <Col lg={12}>
                 <div className="form-floating">
                   <Form.Control type="text" id="firstnamefloatingInput" placeholder="Enter your firstname" />
                   <Form.Label htmlFor="firstnamefloatingInput">Nome</Form.Label>
                 </div>
               </Col>
-              <Col lg={6}>
+              <Col lg={12}>
                 <div className="form-floating">
                   <Form.Control type="text" id="lastnamefloatingInput" placeholder="Enter your Lastname" />
-                  <Form.Label htmlFor="lastnamefloatingInput">Email</Form.Label>
+                  <Form.Label htmlFor="lastnamefloatingInput">CNPJ</Form.Label>
                 </div>
               </Col>
-              <Col lg={10}>
+              <Col lg={12}>
                 <div className="form-floating">
-                  <Form.Control type="email" id="emailfloatingInput" placeholder="Enter your email" />
-                  <Form.Label htmlFor="emailfloatingInput">Endereço</Form.Label>
+                  <Form.Control type="text" id="emailfloatingInput" placeholder="Enter your email" />
+                  <Form.Label htmlFor="emailfloatingInput">Telefone</Form.Label>
                 </div>
-              </Col>
-              <Col lg={2}>
-                <div className="form-floating">
-                  <Form.Control type="password" id="passwordfloatingInput" placeholder="Enter your password" />
-                  <Form.Label htmlFor="passwordfloatingInput">Número</Form.Label>
-                </div>
-              </Col>
-              <Col lg={12} >
-                <Form.Select className='mb-3 form-select-lg' aria-label='exemplo'>
-                  <option defaultValue={0} value="">Selecione o tipo de fornecedor</option>
-                  <option value="1">Plantas</option>
-                  <option value="2">Acessórios</option>
-                </Form.Select>
               </Col>
               <Col lg={12}>
                 <div className="hstack gap-2 justify-content-end">
